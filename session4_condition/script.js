2730ab4f24ff3e1f8691cab24945d45a0258f781
Hung Nguyen Xuan <hungnx@pixelz.com>
2020-02-25 21:54:07 +0700

    session 4 (continue) & session 5

--- session4_condition/script.js
+++ session4_condition/script.js
@@ -11,12 +11,43 @@ if (number === 8) {
 }
 
 let year = parseInt(prompt("Nhập năm"));
-if ((year % 4 === 0 && year % 100 !== 0) 
-|| (year % 100 === 0 && year % 400 === 0)) {
+if ((year % 4 === 0 && year % 100 !== 0)
+  || (year % 100 === 0 && year % 400 === 0)) {
   console.log("Năm nhuận");
-}else{
+} else {
   console.log("Không là năm nhuận");
 }
 
 let month = parseInt(prompt("Nhập vào 1 tháng"));
-if()
+// if (month === 1 || month === 3
+//   || month === 5 || month === 7
+//   || month === 8 || month === 10
+//   || month === 12
+// ) {
+//   console.log("31 ngày");
+// } else if (month === 2) {
+//   console.log("28 hoặc 29 ngày");
+// } else if (month === 4 || month === 6
+//   || month === 9 || month === 11) {
+//   console.log("30 ngày");
+// } else {
+//   console.log("Nhập sai");
+// }
+
+switch (month) {
+  case 1:
+  case 3:
+    console.log("31");
+    break;
+  case 2:
+    console.log("28 hoặc 29");
+    break;
+  case 4:
+    console.log("30");
+    break;
+  case 5:
+    console.log("31");
+    break;
+  default://optional
+    console.log("Nhập sai");
+}
---
+++ session5_loop/loop.js
@@ -0,0 +1,76 @@
+// for (let index = 2; index < 11; index++) {
+//   // console.log("Hello world");
+//   console.log(index);
+// }
+// for (let index = 10; index > 0; index -= 2) {
+//   // console.log("Hello world");
+//   console.log(index);
+// }
+
+// let m = parseInt(prompt("Nhập m:"));
+// let n = parseInt(prompt("Nhập n:"));
+// let a, b;
+// // Math.max(), Math.min();
+// a = (m <= n ? m : n);
+// if (m <= n) {
+//   // for (let i = m; i <= n; i++) {
+//   //   console.log(i);
+//   // }
+//   a = m; b = n;
+// } else {
+//   // for (let i = n; i <= m; i++) {
+//   //   console.log(i);
+//   // }
+//   a = n; b = m;
+// }
+
+//B1: kb: a, b vs a = min(m, n), b = max(m, n);
+//B2: for a => b;
+
+// // Tính tổng
+// let m = 10;
+// let s = 0;
+// for (let i = 0; i <= m; i++) {
+//   s = s + i;// s += i;
+// }
+// console.log(s);
+
+// s = 0;
+// for (let i = 1; i <= M; i++) {
+//   let giaiThua = 1;
+//   for (let j = 1; j <= i; j++) { // Tính giaithừa
+//     giaiThua = giaiThua * j;
+//   }
+//   s = s + giaiThua;
+// }
+// s = 1;
+// let giaiThua = 1;
+// for (let i = 2; i <= M; i++) {
+//   giaiThua = giaiThua * i;
+//   s = s + giaiThua;
+// }
+
+// let n = Number(prompt("Nhập N"));
+// while (n < 8) {
+//   n = Number(prompt("Nhập lại N >= 8"));
+// }
+// console.log(n);
+
+// let pass = prompt("Nhập mk:");
+// while (pass.length < 8) {
+//   pass = prompt("Mk quá ngắn, nhập lại: ");
+// }
+// console.log(pass);
+
+//Nhập số chẵn > 10
+// let n = parseInt(prompt("Nhập N"));
+// while (!(n > 10 && n % 2 === 0)) {
+//   n = parseInt(prompt("Nhập N"));
+// }
+// console.log(n);
+
+let pass = prompt("Nhập Mk");
+if(!(pass.length >= 8 && pass.search('@') >= 0)){
+  pass = prompt("Nhập lại mk");
+}
+console.log(pass);
